77f1570fd6f43adcdda0ad3c735280ae7d640c6b
Christian Foellmann <christian@foellmann.de>
2025-12-31 10:12:59 +0100

    add compression feature

--- exec.sh
+++ exec.sh
@@ -13,35 +13,50 @@ echo "$(date) - start backup scheduler"
 while :; do
     DATE=$(date +%Y%m%d-%H%M%S)
 
-	if [ -z "$GITHUB_USER" ]
-	  then
+    if [ -z "$GITHUB_USER" ]
+      then
       echo "No Github users defined."
     else
       for u in $(echo $GITHUB_USER | tr "," "\n"); do
         echo "$(date) - execute backup for User ${u}, ${DATE}"
-        github-backup ${u} --token=$TOKEN --output-directory=/srv/var/${DATE}/${u} ${BACKUP_OPTIONS}
-      done
-  fi
+        OUTDIR="/srv/var/${DATE}/${u}"
+        ARCHIVE="/srv/var/${DATE}/${u}.tar.gz"
 
-	if [ -z "$GITHUB_ORG" ]
-	  then
+        github-backup "${u}" --token="$TOKEN" --output-directory="$OUTDIR" ${BACKUP_OPTIONS}
+
+        if [ -n "$COMPRESSION" ]; then
+          echo "$(date) - compress ${OUTDIR} -> ${ARCHIVE}"
+          tar -C "/srv/var/${DATE}" -czf "$ARCHIVE" "${u}" && rm -rf "$OUTDIR"
+        fi
+      done
+    fi
+
+    if [ -z "$GITHUB_ORG" ]
+      then
           echo "No Github organization defined."
     else
       for o in $(echo $GITHUB_ORG | tr "," "\n"); do
-        echo "$(date) - execute backup for Organization ${u}, ${DATE}"
-        github-backup ${o} --organization --token=$TOKEN --output-directory=/srv/var/${DATE}/${o} ${BACKUP_OPTIONS}
-      done
-	fi
+        echo "$(date) - execute backup for Organization ${o}, ${DATE}"
+        OUTDIR="/srv/var/${DATE}/${o}"
+        ARCHIVE="/srv/var/${DATE}/${o}.tar.gz"
 
-	if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_ORG" ]
+        github-backup "${o}" --organization --token="$TOKEN" --output-directory="$OUTDIR" ${BACKUP_OPTIONS}
+
+        if [ -n "$COMPRESSION" ]; then
+          echo "$(date) - compress ${OUTDIR} -> ${ARCHIVE}"
+          tar -C "/srv/var/${DATE}" -czf "$ARCHIVE" "${o}" && rm -rf "$OUTDIR"
+        fi
+      done
+    fi
+
+    if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_ORG" ]
     then
       echo "No entities (user, organization) defined. No backup performed."
     else
       echo "Backup performed."
-  fi
-    
-    echo "$(date) - cleanup"
+    fi
 
+    echo "$(date) - cleanup"
     ls -d1 /srv/var/* | head -n -${MAX_BACKUPS} | xargs rm -rf
 
     echo "$(date) - sleep for 1 day"
